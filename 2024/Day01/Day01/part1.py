0ad6cdd4a05cf17f2c1740019d503234153bb189
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-12-01 21:55:13 +0100

    Day 01 resolved :star:  :star:

---
+++ 2024/Day01/Day01/part1.py
@@ -0,0 +1,26 @@
+﻿
+if __name__ == '__main__':
+
+    #load puzzle
+    with open('puzzle.txt') as file:
+        puzzles = file.read().split('\n')
+
+        left = []
+        right = []
+
+        # split puzzles into 2 lists
+        for puzzle in puzzles:
+            left.append(int(puzzle.split()[0]))
+            right.append(int(puzzle.split()[1]))
+
+        # sort
+        left.sort()
+        right.sort()
+
+        puzzles =  [left, right]
+        result = 0
+
+        for i in range(0, len(left)):
+            result += abs(left[i] - right[i])
+
+        print(f'Result: {result}')
