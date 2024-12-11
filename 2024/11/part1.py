9678f6da3cbb51c486965e6dd2fb745a985a7842
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-12-11 23:12:01 +0100

    Day 11 ⭐⭐

---
+++ 2024/11/part1.py
@@ -0,0 +1,30 @@
+﻿with open('puzzle.txt', 'r') as puzzle:
+    stones = [int(s) for s in puzzle.read().split()]
+    counter = 0
+
+    while counter < 25:
+        print(f'Counter: {counter}...')
+        buffer = list()
+        for index, stone in enumerate(stones):
+            # if the stone is engraved with the number 0
+            if stone == 0:
+                # it is replaced by a stone engraved with the number 1.
+                buffer.append(1)
+                continue
+
+            # if the stone is engraved with a number that has an even number of digits.
+            if len(str(stone)) % 2 == 0:
+                # it is replaced by two stones, e.g. 253000 -> 253 000
+                size = len(str(stone)) // 2
+                stone_one = int(''.join([i for i in str(stone)[:size]]))
+                stone_two = int(''.join([i for i in str(stone)[size:]]))
+                buffer.extend([stone_one, stone_two])
+                continue
+            # if none of the other rules apply, the stone is replaced by a new stone;
+            # the old stone's number multiplied by 2024
+            buffer.append(stones[index] * 2024)
+
+        stones = buffer.copy()
+        counter += 1
+
+    print(f'Result: {len(stones)}')
---
+++ 2024/11/part2.py
@@ -0,0 +1,27 @@
+﻿from functools import cache
+
+@cache
+def calculate(current_stone, iterations) -> int:
+    if iterations == 0:
+        return 1
+    # if the stone is engraved with the number 0 it is replaced by a stone engraved with the number 1
+    if current_stone == 0:
+        return calculate(1, iterations - 1)
+
+    if len(str(current_stone)) % 2 == 0:
+        size = len(str(current_stone)) // 2
+        stone_one = int(''.join([i for i in str(current_stone)[:size]]))
+        stone_two = int(''.join([i for i in str(current_stone)[size:]]))
+
+        return calculate(stone_one, iterations - 1) + calculate(stone_two, iterations - 1)
+
+    return calculate(current_stone * 2024, iterations - 1)
+
+with open('puzzle.txt', 'r') as puzzle:
+    stones = [int(s) for s in puzzle.read().split()]
+    result = 0 
+
+    for stone in stones:
+        result +=  calculate(stone, 75)
+
+    print(f'Result: {result}')
